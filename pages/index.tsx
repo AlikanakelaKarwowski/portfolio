efd7bd63a50b6a27885e67540c91fb3fab2130bb
A.Karwowski <akk42@zips.uakron.edu>
2023-03-30 15:53:59 -0600

    add hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <div>
+        <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
             <Head>
                 <title>Alexanders Portfolio</title>
             </Head>
             <Header />
-            <section id="hero"></section>
+            <section id="hero" className="snap-center">
+                <Hero />
+            </section>
             {/* Hero */}
             {/* About */}
             {/* Experience */}
