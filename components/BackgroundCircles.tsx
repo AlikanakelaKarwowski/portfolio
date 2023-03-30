293dd082ebd36be13305a2396faf0ea42f908b14
A.Karwowski <akk42@zips.uakron.edu>
2023-03-30 16:28:20 -0600

    Change the border width and remove the scale animation cause i dont think it looks good on a 2nd go around

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -9,7 +9,6 @@ function BackgroundCircles({}: Props) {
                 opacity: 0,
             }}
             animate={{
-                scale: [1, 2, 2, 2, 1],
                 opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
             }}
             transition={{
@@ -17,11 +16,11 @@ function BackgroundCircles({}: Props) {
             }}
             className="relative flex justify-center items-center"
         >
-            <div className="absolute border-2 border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-            <div className="absolute border-2 border-[#333333] rounded-full h-[300px] w-[300px] mt-52 animate-ping" />
-            <div className="absolute border-2 border-[#333333] rounded-full h-[500px] w-[500px] mt-52 animate-ping" />
+            <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
+            <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 animate-ping" />
+            <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 animate-ping" />
             <div className="absolute border-4 border-[#F7AB0A] rounded-full opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
-            <div className="absolute border-2 border-[#333333] rounded-full h-[800px] w-[800px] mt-52 animate-ping" />
+            <div className="absolute border border-[#333333] rounded-full h-[800px] w-[800px] mt-52 animate-ping" />
         </motion.div>
     );
 }
