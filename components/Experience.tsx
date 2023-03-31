61bcd74188d5184843548a90e2ef7fa4cdaf6574
A.Karwowski <akk42@zips.uakron.edu>
2023-03-31 15:05:48 -0600

    Add style to scrollbar

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,7 +12,7 @@ function Experience({}: Props) {
             className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
         >
             <h3 className="sectionHeader">Experience</h3>
-            <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+            <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]">
                 <ExperienceCard />
                 <ExperienceCard />
                 <ExperienceCard />
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,13 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+        <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]">
             <Head>
                 <title>Alexanders Portfolio</title>
             </Head>
