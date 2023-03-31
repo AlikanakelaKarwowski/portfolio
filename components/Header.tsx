e4e8393c86f166fdcddcc11ef068f8f1af2f9690
A.Karwowski <akk42@zips.uakron.edu>
2023-03-31 15:24:05 -0600

    fixed issue with rendering social icon using link

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { EnvelopeIcon } from "@heroicons/react/24/solid";
 type Props = {};
 
 export default function Header({}: Props) {
@@ -23,32 +24,31 @@ export default function Header({}: Props) {
                 }}
                 className="flex flex-row items-center"
             >
-                {/* {Social Icons} */}
                 <SocialIcon url="https://www.youtube.com/@pchelphubdiscord" fgColor="gray" bgColor="transparent" />
                 <SocialIcon url="https://www.youtube.com/@pchelphubdiscord" fgColor="gray" bgColor="transparent" />
                 <SocialIcon url="https://www.youtube.com/@pchelphubdiscord" fgColor="gray" bgColor="transparent" />
             </motion.div>
-            <motion.div
-                initial={{
-                    x: 500,
-                    opacity: 0,
-                    scale: 0.5,
-                }}
-                animate={{
-                    x: 0,
-                    opacity: 1,
-                    scale: 1,
-                }}
-                transition={{
-                    duration: 1.5,
-                }}
-                className="flex flex-row items-center text-gray-300 cursor-pointer"
-            >
-                <Link href="#contact">
-                    <SocialIcon className="cursor-pointer" network="email" fgColor="gray" bgColor="transparent" />
+            <Link href="#contact">
+                <motion.div
+                    initial={{
+                        x: 500,
+                        opacity: 0,
+                        scale: 0.5,
+                    }}
+                    animate={{
+                        x: 0,
+                        opacity: 1,
+                        scale: 1,
+                    }}
+                    transition={{
+                        duration: 1.5,
+                    }}
+                    className="flex flex-row items-center text-gray-300 cursor-pointer"
+                >
+                    <EnvelopeIcon className="cursor-pointer mx-2 text-[#F7AB0A] h-7 w-7 animate-pulse" />
                     <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
-                </Link>
-            </motion.div>
+                </motion.div>
+            </Link>
         </header>
     );
 }
