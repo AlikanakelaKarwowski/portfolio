9149f373fb1d76fe76422bfa65ef626c58217650
A.Karwowski <akk42@zips.uakron.edu>
2023-04-04 15:46:31 -0600

    pass in props to skill and skills component

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
-type Props = { directionLeft?: boolean };
+import { Skill } from "@/typings";
+import { urlFor } from "@/sanity";
+type Props = {
+    skill: Skill;
+    directionLeft?: boolean;
+};
 
-function Skill({ directionLeft }: Props) {
+function Skill({ skill, directionLeft }: Props) {
     return (
         <div className="group relative flex cursor-pointer">
             <motion.img
@@ -12,13 +17,13 @@ function Skill({ directionLeft }: Props) {
                 }}
                 transition={{ duration: 1 }}
                 whileInView={{ opacity: 1, x: 0 }}
-                src="https://cdn-icons-png.flaticon.com/512/5968/5968350.png"
+                src={urlFor(skill.image).url()}
                 alt=""
                 className="rounded-full border border-gray-500 object-cover w-24 h-24 md:h-28 md:w-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
             />
-            <div className="absolute opacity-0 group-hover:opacity-80 transition durationi-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
                 <div className="flex items-center justify-center h-full">
-                    <p className="text-3xl font-bold text-black opacity-100 ">100%</p>
+                    <p className="text-2xl font-bold text-black opacity-100 ">{skill.progress} years</p>
                 </div>
             </div>
         </div>
