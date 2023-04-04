b6a467af8c935b7a5a260ec39cb35bf9d4b38b0a
A.Karwowski <akk42@zips.uakron.edu>
2023-04-04 13:24:23 -0600

    pass props in and map over socials

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,12 @@ import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { EnvelopeIcon } from "@heroicons/react/24/solid";
-type Props = {};
+import { Social } from "@/typings";
+type Props = {
+    socials: Social[];
+};
 
-export default function Header({}: Props) {
+export default function Header({ socials }: Props) {
     return (
         <header className="sticky p-5 top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
             <motion.div
@@ -24,9 +27,9 @@ export default function Header({}: Props) {
                 }}
                 className="flex flex-row items-center"
             >
-                <SocialIcon url="https://www.youtube.com/@pchelphubdiscord" fgColor="gray" bgColor="transparent" />
-                <SocialIcon url="https://www.youtube.com/@pchelphubdiscord" fgColor="gray" bgColor="transparent" />
-                <SocialIcon url="https://www.youtube.com/@pchelphubdiscord" fgColor="gray" bgColor="transparent" />
+                {socials.map((social) => (
+                    <SocialIcon key={social._id} url={social.url} fgColor="gray" bgColor="transparent" />
+                ))}
             </motion.div>
             <Link href="#contact">
                 <motion.div
